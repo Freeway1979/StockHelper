0dea85936e5999171c62f2575ca6239021d9e033
Andy Liu <andyli2@cisco.com>
2018-09-19 18:06:16 +0800

    Change Author

--- StockHelper/StockHelper/Feature/Block/Model/StockBlockChange.swift
+++ StockHelper/StockHelper/Feature/Block/Model/StockBlockChange.swift
@@ -2,7 +2,7 @@
 //  StockBlockChange.swift
 //  StockHelper
 //
-//  Created by andyli2 on 2018/9/19.
+//  Created by Andy Liu on 2018/9/19.
 //  Copyright © 2018 Andy Liu. All rights reserved.
 //
 
